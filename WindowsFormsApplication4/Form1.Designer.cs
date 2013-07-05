fb3ecc47491765b02768395bc2dd47bf79df37d3
Scott Chang <acdcpurmac@gmail.com>
2013-07-04 20:09:59 -0400

    First Commit

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.chooseFoler = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Enabled = false;
+            this.btnCapture.Location = new System.Drawing.Point(93, 67);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(103, 31);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // chooseFoler
+            // 
+            this.chooseFoler.Location = new System.Drawing.Point(93, 12);
+            this.chooseFoler.Name = "chooseFoler";
+            this.chooseFoler.Size = new System.Drawing.Size(103, 23);
+            this.chooseFoler.TabIndex = 1;
+            this.chooseFoler.Text = "Choose";
+            this.chooseFoler.UseVisualStyleBackColor = true;
+            this.chooseFoler.Click += new System.EventHandler(this.chooseFoler_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 41);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(103, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time Interval";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 118);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.chooseFoler);
+            this.Controls.Add(this.btnCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Capture!";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button chooseFoler;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fb;
+        bool isCapture;
+        public Form1()
+        {
+            InitializeComponent();
+            fb = new FolderBrowserDialog();
+            isCapture = false;
+            timer1.Interval = (int)numericUpDown1.Value * 60000;
+        }
+
+        private void CaptureImage()
+        {
+            DateTime currentTime = DateTime.Now;
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, bounds.Size);
+                }
+                string datePath = String.Format("{0}_{1}_{2}", currentTime.Month, currentTime.Day, currentTime.Year);
+                string directoryPath = System.IO.Path.Combine(fb.SelectedPath, datePath);
+                System.IO.Directory.CreateDirectory(directoryPath);
+
+                string filename = String.Format("{0}_{1}_{2}.png", currentTime.Hour, currentTime.Minute, currentTime.Second);
+                string savePath = System.IO.Path.Combine(directoryPath, filename);
+                //System.Diagnostics.Debug.WriteLine(savePath);
+                bitmap.Save(savePath, ImageFormat.Png);
+            }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            if (!isCapture)
+            {
+                isCapture = true;
+                CaptureImage();
+                timer1.Enabled = true;
+                btnCapture.Text = "Stop";
+            }
+            else
+            {
+                isCapture = false;
+                timer1.Enabled = false;
+                btnCapture.Text = "Capture";
+            }
+            
+        }
+
+        private void chooseFoler_Click(object sender, EventArgs e)
+        {            
+            DialogResult result =  fb.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                btnCapture.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CaptureImage();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value * 60000;
+        }
+    }
+}
